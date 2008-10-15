1ae9003c910de2f5a3792e0bc8e89ce1ef526a18
Marcus Ramberg <marcus@nordaaker.com>
2008-10-15 21:13:32 +0000

    update version numbers in .pm files

--- lib/Catalyst.pm
+++ lib/Catalyst.pm
@@ -63,7 +63,7 @@ __PACKAGE__->stats_class('Catalyst::Stats');
 
 # Remember to update this in Catalyst::Runtime as well!
 
-our $VERSION = '5.7014';
+our $VERSION = '5.7015';
 
 sub import {
     my ( $class, @arguments ) = @_;
